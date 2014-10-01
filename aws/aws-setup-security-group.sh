4ad62269c95ea456626d997bc4db0d525adde71e
Brady Doll <bradydoll@gmail.com>
2014-10-01 12:30:32 -0400

    Add prompt for secruity group name (defaults to 'wirbelsturm').
    Add support for VPC (currently commented out).

--- aws/aws-setup-security-group.sh
+++ aws/aws-setup-security-group.sh
@@ -10,8 +10,7 @@ puts "+---------------------------------------------+"
 puts "| CREATING AWS SECURITY GROUP FOR WIRBELSTURM |"
 puts "+---------------------------------------------+"
 
-SECURITY_GROUP="wirbelsturm"
-VERSION="1.0"
+VERSION="1.1"
 
 warn
 warn "Note: By default each Wirbelsturm machine runs its own local firewall."
@@ -25,48 +24,67 @@ warn "    default setting (0.0.0.0/0) allows SSH access from any IP."
 
 read -e -p "Source CIDR [0.0.0.0/0]: " SOURCE_CIDR
 
-if [ -z "$SOURCE_CIDR" ]; then
+if [ -z $SOURCE_CIDR ]; then
   SOURCE_CIDR="0.0.0.0/0"
 fi
 
+read -e -p "Security group name [wirbelsturm]: " SECURITY_GROUP
+
+if [ -z $SECURITY_GROUP ]; then
+  SECURITY_GROUP="wirbelsturm"
+fi
+
+#read -e -p "VPC ID (Optional) []: " VPC_ID
+
 puts "Creating '$SECURITY_GROUP' security group for Wirbelsturm..."
-aws ec2 create-security-group --group-name $SECURITY_GROUP --description "Wirbelsturm cluster (security policy v$VERSION)"
+#if [ -z $VPC_ID ]; then
+	SG_OUT=`aws ec2 create-security-group --group-name $SECURITY_GROUP --description "Wirbelsturm cluster (security policy v$VERSION)"`
+#else
+#	SG_OUT=`aws ec2 create-security-group --group-name $SECURITY_GROUP --description "Wirbelsturm cluster (security policy v$VERSION)" --vpc-id $VPC_ID`
+#fi
+
 if [ $? -ne 0 ]; then
   warn "Note: If you want to delete the security group you can do so with:"
   warn
   warn "    $ aws ec2 delete-security-group --group-name $SECURITY_GROUP"
   warn
+  warn "Note: VPC security groups cannot be deleted by group name. Instead use the following command substituting the secruity group ID for GROUP_ID:"
+  warn
+  warn "    $ aws ec2 delete-security-group --group-id GROUP_ID"
+  warn
   warn "DELETING A GROUP WILL DELETE ALL OF ITS RULES AND IS NOT REVERSIBLE."
   exit 1
 fi
 
+GROUP_ID=`echo $SG_OUT | jq --raw-output '.GroupId'`
+
 puts "Enable SSH access"
-aws ec2 authorize-security-group-ingress --protocol tcp --port 22 --cidr $SOURCE_CIDR --group-name $SECURITY_GROUP || exit 1
+aws ec2 authorize-security-group-ingress --protocol tcp --port 22 --cidr $SOURCE_CIDR --group-id $GROUP_ID > /dev/null || exit 1
 
 puts "Enable access to Storm master"
 # 6627 (thrift/Nimbus)
 # 8080 (UI)
-aws ec2 authorize-security-group-ingress --protocol tcp --port 6627 --source-group $SECURITY_GROUP --group-name $SECURITY_GROUP || exit 1
-aws ec2 authorize-security-group-ingress --protocol tcp --port 8080 --source-group $SECURITY_GROUP --group-name $SECURITY_GROUP || exit 1
+aws ec2 authorize-security-group-ingress --protocol tcp --port 6627 --source-group $GROUP_ID --group-id $GROUP_ID > /dev/null || exit 1
+aws ec2 authorize-security-group-ingress --protocol tcp --port 8080 --source-group $GROUP_ID --group-id $GROUP_ID > /dev/null || exit 1
 
 puts "Enable access to Storm slaves"
 # 3772 (drpc), 3773 (drpc invocations)
 # 67xx supervisor ports
-aws ec2 authorize-security-group-ingress --protocol tcp --port 3772-3773 --source-group $SECURITY_GROUP --group-name $SECURITY_GROUP || exit 1
-aws ec2 authorize-security-group-ingress --protocol tcp --port 6700-6799 --source-group $SECURITY_GROUP --group-name $SECURITY_GROUP || exit 1
+aws ec2 authorize-security-group-ingress --protocol tcp --port 3772-3773 --source-group $GROUP_ID --group-id $GROUP_ID > /dev/null || exit 1
+aws ec2 authorize-security-group-ingress --protocol tcp --port 6700-6799 --source-group $GROUP_ID --group-id $GROUP_ID > /dev/null || exit 1
 
 puts "Enable access to Kafka"
-aws ec2 authorize-security-group-ingress --protocol tcp --port 9092 --source-group $SECURITY_GROUP --group-name $SECURITY_GROUP || exit 1
+aws ec2 authorize-security-group-ingress --protocol tcp --port 9092 --source-group $GROUP_ID --group-id $GROUP_ID > /dev/null || exit 1
 
 puts "Enable access to Zookeeper"
-aws ec2 authorize-security-group-ingress --protocol tcp --port 2181 --source-group $SECURITY_GROUP --group-name $SECURITY_GROUP || exit 1
+aws ec2 authorize-security-group-ingress --protocol tcp --port 2181 --source-group $GROUP_ID --group-id $GROUP_ID > /dev/null || exit 1
 
 puts "Enable access to Redis"
-aws ec2 authorize-security-group-ingress --protocol tcp --port 6379 --source-group $SECURITY_GROUP --group-name $SECURITY_GROUP || exit 1
+aws ec2 authorize-security-group-ingress --protocol tcp --port 6379 --source-group $GROUP_ID --group-id $GROUP_ID > /dev/null || exit 1
 
 puts "------------------------------------------------------------------------"
 puts "Summary of security group:"
-aws ec2 describe-security-groups --group-names $SECURITY_GROUP
+aws ec2 describe-security-groups --group-ids $GROUP_ID
 puts "------------------------------------------------------------------------"
 
 success "You can now use the AWS security group '$SECURITY_GROUP' for your Wirbelsturm cluster."
