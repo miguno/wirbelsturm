6c6625963d6677aa938fd9757430636836582aa0
scottrutherford <scott@thatsuseful.com>
2014-11-12 00:28:39 +0000

    more on provisioning

--- aws/aws-create-ami.sh
+++ aws/aws-create-ami.sh
@@ -27,28 +27,38 @@ fi
 
 SI_OUT=`aws --profile $PROFILE ec2 --region $REGION run-instances \
     --key-name wirbelsturm \
-    --instance-type t1.micro \
+    --instance-type m3.medium \
     --block-device-mappings "[{\"DeviceName\":\"/dev/sda1\",\"Ebs\":{\"DeleteOnTermination\":true,\"VolumeSize\":40,\"VolumeType\":\"io1\",\"Iops\":400}}]" \
     --security-groups wirbelsturm \
     --image-id $AMI` || exit 1
 INSTANCE_ID=`echo $SI_OUT | jq --raw-output '.Instances[0].InstanceId'`
 puts "InstanceId: $INSTANCE_ID"
-puts "Waiting for instance to resolve host"
-sleep 20
 
-DI_OUT=`aws --profile $PROFILE ec2 --region $REGION describe-instances --instance-ids $INSTANCE_ID` || exit 1
-HN=`echo $DI_OUT | jq --raw-output '.Reservations[0].Instances[0].PublicDnsName'`
+puts "Waiting for instance to assign public DNS name"
+
+HN="null"
+while [ $HN=="null" ] 
+do
+  sleep 1
+
+  DI_OUT=`aws --profile $PROFILE ec2 --region $REGION describe-instances --instance-ids $INSTANCE_ID`
+  puts $DI_OUT
+
+  HN=`echo $DI_OUT | jq --raw-output '.Reservations[0].Instances[0].PublicDnsName'`
+  puts $HN
+done
+puts $HN
 
 puts "Hostname: '$HN'"
 puts "Waiting for '$HN' to become available"
-sleep 60
-
-scp -i ~/.ssh/wirbelsturm.pem aws-prepare-image.sh puppetlabs.repo ec2-user@$HN:~
+until scp -i ~/.ssh/wirbelsturm.pem aws-prepare-image.sh puppetlabs.repo ec2-user@$HN:~ > /dev/null 2>&1
+  do sleep 1
+done
 ssh -t -i ~/.ssh/wirbelsturm.pem ec2-user@$HN '~/aws-prepare-image.sh'
 
 puts "Creating image..."
-CI_OUT=`aws --profile $PROFILE ec2 --region $REGION create-image --name wirbelsturm-base-$INSTANCE_ID \
-	--description 'Stock ami-55a7ea65 (Amazon Linux 2014.09.1) with Puppet 3.7.x and fix for vagrant-aws issue #72' \
+CI_OUT=`aws --profile $PROFILE ec2 --region $REGION create-image --name wirbelsturm-base-$AMI-$INSTANCE_ID \
+	--description 'Amazon Linux 2014.09.1 with Puppet 3.7.x and fix for vagrant-aws issue #72' \
 	--instance-id $INSTANCE_ID`
 IMAGE_ID=`echo $CI_OUT | jq --raw-output '.ImageId'`
 
